c36b56e179a0fbe5a9f8fcb24104744c50b19662
Rodolfo Navarro <rodnavarroc@outlook.com>
2023-09-29 17:11:34 -0600

    Update recent projects and UI

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -1,20 +1,56 @@
 import React from 'react'
 
-const ProjectCard = ({image, name, description}) => {
+const ProjectCard = ({ image, name, description, stack, url, adultsOnly }) => {
+
+  const renderStackElement = (e) => {
+    switch (e) {
+      case 'NextJS': {
+        return <img src="https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png" />
+      }
+      case 'PHP': {
+        return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png" />
+      }
+      case 'SQL': {
+        return <img src="https://play-lh.googleusercontent.com/hvK9JjjMrQ-MSP98UVqmwpgojkc89P5tYvLUbvbnAqORVx3o7mUhk_NNdSD4S9_F8pw" />
+      }
+      case 'SASS': {
+        return <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1200px-Sass_Logo_Color.svg.png" />
+      }
+      case 'AWS': {
+        return <img src="https://www.c3ntro.com/hubfs/logo-aws-blanco%20(1).png" />
+      }
+    }
+  }
+
   return (
     <div className="project-card-container">
 
       <div className="image">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ28WA2ZQREgEZ1jva2HNK6hzzNLXtnkxGhG2eCg1bAuw&s" alt={name} />
+        <img src={image} alt={name} />
       </div>
 
       <div className='card-project-body'>
         <h3>{name}</h3>
-        <h5>{description}</h5>
+        <h5>{description}
+          {adultsOnly ?
+            <>
+              <br /><br />
+              <b style={{color: 'goldenrod', fontSize: '12px'}}>NSFW Alert! This is an adult shop website. </b>
+            </>
+            : null
+          }
+        </h5>
 
         <h6>TECH STACK</h6>
+        <div className="tech-stack-container">
+          {stack.map((e) =>
+            renderStackElement(e)
+          )}
+        </div>
 
-        <span className='checkitout-button'>Check it out 👉🏻</span>
+        <div className="checkitout-button" onClick={() => window.open(url, '_blank')}>
+          <b>Visit website</b>
+        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import Navbar from '../components/Navbar/Navbar'
 import ProjectCard from '../components/ProjectCard/ProjectCard'
 import SkillCard from '../components/SkillCard/SkillCard'
@@ -6,6 +7,9 @@ import SkillCard from '../components/SkillCard/SkillCard'
 const index = () => {
   return (
     <>
+      <Head>
+        <title>Rodolfo Navarro | Software Enginner</title>
+      </Head>
       <Navbar />
       <div className="index-container">
 
@@ -14,9 +18,9 @@ const index = () => {
             <h2>Hello! 👋🏻 I&apos;m a fullstack web developer <b> based in Mexico 🇲🇽</b></h2>
             <h4>This is a quick view on me and my work. 👇🏻</h4>
             <div className="contact">
-              <span>Keep scrolling,</span>
-              <span>or maybe you want to</span>
-              <span style={{cursor: 'pointer'}}><a href="mailto:rodnavarroc@outlook.com">Get in touch ✨</a></span>
+              <span>Keep scrolling</span>
+              <span>or</span>
+              <span style={{ cursor: 'pointer' }}><a href="mailto:rodnavarroc@outlook.com">Get in touch via email ✨</a></span>
             </div>
           </div>
         </div>
@@ -31,25 +35,37 @@ const index = () => {
             </div>
 
             <div className="body">
-            <ProjectCard
+              <ProjectCard
                 name="Venus"
                 description="E-commerce website for a Sex Shop brand where I was part of the UI / UX team, and back-end development."
-                image=""
+                image="/venus.jpg"
+                stack={['NextJS', 'SASS', 'PHP', 'SQL', 'AWS']}
+                url="https://www.tiendavenus.com/"
+                adultsOnly={true}
               />
               <ProjectCard
                 name="Tres Distribuciones"
                 description="E-commerce website for a Sexual Health & Entertainment products wholesale brand where I was part of the UI / UX and full stack development teams."
-                image=""
+                image="/tres distribuciones.jpg"
+                stack={['NextJS', 'SASS', 'PHP', 'SQL', 'AWS']}
+                url="https://www.tresdistribuciones.com/"
+                adultsOnly={true}
               />
               <ProjectCard
                 name="Odibo"
-                description="E-commerce website for an adult toys and accesories brand where I was part of the back-end dev team."
-                image=""
+                description="E-commerce website for an adult toys brand where I was part of the back-end dev team."
+                image="/odibo.jpg"
+                stack={['NextJS', 'SASS', 'PHP', 'SQL', 'AWS']}
+                url="https://www.odibo.mx/"
+                adultsOnly={true}
               />
               <ProjectCard
                 name="Sey"
                 description="E-commerce like website for an adult toys and accesories brand where I was part of the UI / UX team, SEO improvement and back-end development."
-                image=""
+                image="/sey.jpg"
+                stack={['NextJS', 'SASS', 'PHP', 'SQL', 'AWS']}
+                url="https://www.sey.mx/"
+                adultsOnly={true}
               />
             </div>
 
